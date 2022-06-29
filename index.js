c57432a438029b55233e9add934782637a28c0ff
Elisa <emsp.star@gmail.com>
2022-06-29 15:51:04 +0200

    Base proyecto BE

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.get("/", (req, res) => {
+    res.status(200)
+        .json({ message: "ok" });
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
